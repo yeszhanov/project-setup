ed215b3e1acb510591625cb99728c694705aa8a1
eneszhanov <eneszhanov@kassa24.kz>
2021-02-14 04:43:50 +0600

    ADDED: babel loader, css loader

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-const elven = 'test'
+import "./index.css"
+const elven = {name:2}
+const lala = {...elven, test:3}
 
 console.log(elven);
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const MiniCssExtractPlugin =require("mini-css-extract-plugin")
+let mode = 'development'
+
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production'
+}
+module.exports={
+  mode:mode,
+  module:{
+    rules:[
+      {
+        test:/\.css/i,
+        use:[MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test:/\.js$/,
+        exclude:/node_modules/,
+        use:{
+          loader:'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins:[new MiniCssExtractPlugin()],
+  devtool:'source-map',
+  devServer:{
+    contentBase: "./dist",
+    hot:true
+  }
+}
