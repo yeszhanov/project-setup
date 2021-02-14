0d70c74137821f5aaa81efeea9d006793237dd75
eneszhanov <eneszhanov@kassa24.kz>
2021-02-15 00:06:54 +0600

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,15 +10,27 @@ module.exports = {
     sourceType: 'module',
   },
   extends: [
-    'airbnb',
+    // 'airbnb',
+    'eslint:recommended',
     'plugin:react/recommended',
     'plugin:prettier/recommended',
   ],
 
   plugins: ['react', 'react-hooks'],
+  ignorePatterns: ['/node_modules/**', '/build/**'],
+
   rules: {
     'react/prop-types': 1,
     'linebreak-style': 0,
-    // 'no-unused-vars': 'warn',
+    // 'no-unused-vars': [
+    //   'warn',
+    //   { args: 'none', argsIgnorePattern: 'req|res|next|val' },
+    // ],
+    'prettier/prettier': ['error'],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 };
