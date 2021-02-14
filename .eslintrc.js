9a2ac51bea60a9c9e6e6e4f56ab8aaed1c54a0f2
eneszhanov <eneszhanov@kassa24.kz>
2021-02-15 00:09:07 +0600

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,10 +22,10 @@ module.exports = {
   rules: {
     'react/prop-types': 1,
     'linebreak-style': 0,
-    // 'no-unused-vars': [
-    //   'warn',
-    //   { args: 'none', argsIgnorePattern: 'req|res|next|val' },
-    // ],
+    'no-unused-vars': [
+      'warn',
+      { args: 'none', argsIgnorePattern: 'req|res|next|val' },
+    ],
     'prettier/prettier': ['error'],
   },
   settings: {
